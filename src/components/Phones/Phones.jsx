bc8c2c3c9174bf2286afaf5a300e19bb25108c61
Arman Hossain Somoy <61237257+armansomoy@users.noreply.github.com>
2024-03-13 01:29:09 +0600

    loader

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,37 +1,69 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { Audio } from "react-loader-spinner";
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
     //   .then((res) => res.json())
     //   .then((data) => setPhones(data));
 
-    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-    .then( data => {
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
         const phoneData = data.data.data;
-        const phonesWithFakeData = phoneData.map(phone => {
-            const obj ={
-                name: phone.phone_name,
-                price: parseInt(phone.slug.split('-')[1])
-            }
-            return obj;
-        })
+        const phonesWithFakeData = phoneData.map((phone) => {
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("-")[1]),
+          };
+          return obj;
+        });
         setPhones(phonesWithFakeData);
-        console.log(phonesWithFakeData)
-    })
+        console.log(phonesWithFakeData);
+        setLoading(false)
+      });
   }, []);
-  return <div>
-    <h2>Phones :{phones.length} </h2>
-    <BarChart width={1200} height={400} data={phones}>
-        <Bar dataKey='price' fill="#ffff"></Bar>
-        <XAxis dataKey="name">  </XAxis>
-    </BarChart>
-  </div>;
+  return (
+    <div>
+      <h2>Phones :{phones.length} </h2>
+      {
+        loading && <div>
+
+<Audio
+        height="100"
+        width="100"
+        color="#4fa94d"
+        ariaLabel="audio-loading"
+        wrapperStyle={{}}
+        wrapperClass="wrapper-class"
+        visible={true}
+      />
+        </div>
+      }
+    
+
+      
+      <BarChart width={1200} height={400} data={phones}>
+        <Bar dataKey="price" fill="#ffff"></Bar>
+        <XAxis dataKey="name"> </XAxis>
+      </BarChart>
+    </div>
+  );
 };
 
 export default Phones;
